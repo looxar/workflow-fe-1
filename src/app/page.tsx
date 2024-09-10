787581f63c182be1b9d585d112f72cf5632d26c2
bazsup <bas@odds.team>
2024-09-10 08:24:34 +0700

    budget request list and add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,126 +1,53 @@
 "use client";
-// import ComponentProps from "@/components/ComponentProps";
-import EventProps from "@/components/EventProps";
-import ListRender from "@/components/ListRender";
-import TaskList from "@/components/TaskList";
-import ObjectRender from "@/components/ObjectRender";
-import StatePrimitive from "@/components/StatePrimitive";
-import StateObject from "@/components/StateObject";
+
+import { ChangeEvent, FormEvent, useState } from "react";
+import BudgetPanel from "@/components/BudgetPanel";
+import BudgetRequestDataTable from "../components/BudgetRequestDataTable";
+import Header from "@/components/Header";
+import { BudgetRequest } from "@/models/budget-request";
+import FormAddRequest from "@/components/FormAddRequest";
 
 function Home() {
-  const task = {
-    title: "เบิกงบ",
-    amount: 20,
+  const [budgetRequests, setBudgetRequests] = useState<BudgetRequest[]>([
+    {
+      id: 1,
+      title: "Monitor",
+      amount: 100,
+      quantity: 1,
+      status: "PENDING",
+    },
+    {
+      id: 2,
+      title: "Ram",
+      amount: 200,
+      quantity: 1,
+      status: "APPROVED",
+    },
+    // {
+    //   id: 3,
+    //   title: "CPU",
+    //   amount: 300,
+    //   quantity: 1,
+    //   status: "APPROVED",
+    // },
+  ]);
+  const addRequest = (newRequest: BudgetRequest) => {
+    setBudgetRequests([...budgetRequests, newRequest]);
   };
+
   return (
     <div>
-      {/* <StatePrimitive /> */}
-      <StateObject />
-      {/* <ListRender /> */}
-      {/* <TaskList /> */}
-      {/* <ComponentProps />  */}
-      {/* <ObjectRender title={task.title} amount={100} /> */}
-
-      {/* <EventProps /> */}
-      {/* <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Image
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Title
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Amount
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <Body />
-      </table> */}
+      <Header />
+      <main className="container mx-auto">
+        <div className="mt-4">
+          <BudgetPanel items={budgetRequests} />
+        </div>
+        <FormAddRequest addRequest={addRequest} />
+        <div className="mt-4">
+          <BudgetRequestDataTable items={budgetRequests} />
+        </div>
+      </main>
     </div>
-  );
-}
-
-function Body() {
-  return (
-    <tbody className="bg-white divide-y divide-gray-200">
-      <tr>
-        <td className="px-6 py-4 whitespace-nowrap">
-          <div style={{ height: 112, position: "relative" }}>
-            <Image
-              src="https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg"
-              fill
-              sizes="12vw"
-              alt="Monitor"
-              style={{ objectFit: "contain" }}
-              priority
-            />
-          </div>
-          <img
-            src="https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg"
-            alt="Monitor"
-            className="max-w-full h-28 object-cover rounded"
-          />
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-          Monitor
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-          $2500.00
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-          <div className="flex space-x-2">
-            <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-              Approve
-            </button>
-            <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-              Reject
-            </button>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td className="px-6 py-4 whitespace-nowrap">
-          <img
-            src="https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg"
-            alt="Hard Disk/SSD"
-            className="max-w-full h-28 object-cover rounded"
-          />
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-          Hard Disk/SSD
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-          $2000.75
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-          <div className="flex space-x-2">
-            <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-              Approve
-            </button>
-            <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-              Reject
-            </button>
-          </div>
-        </td>
-      </tr>
-    </tbody>
   );
 }
 
